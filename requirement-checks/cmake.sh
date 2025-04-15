9c539b77ab49025f94d83d4586b3876eb3bd154c
Taylan Gökkaya <insomnimus@proton.me>
2025-04-15 11:15:25 +0300

    fix: update requirement-checks/cmake for the new version

--- requirement-checks/cmake.sh
+++ requirement-checks/cmake.sh
@@ -11,7 +11,7 @@ IFS="." read -r major minor _patch <<<"$version"
 if [[ -z $major ]]; then
 	echo "error: failed to determine cmake version"
 	exit 1
-elif [[ $major -lt 3 || ${minor:-0} -lt 22 ]]; then
+elif [[ $major -lt 3 ]]; then
 	echo "error: your cmake version is too old: $version; required cmake >= 3.22.0"
 	exit 1
 fi
