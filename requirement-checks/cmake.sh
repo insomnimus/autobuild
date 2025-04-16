3697dbe729d43760fdcf0d6b974bc6b8fbed1beb
Taylan Gökkaya <insomnimus@proton.me>
2025-04-16 18:20:23 +0300

    feat: added a hint about --install-cmake to requirement-checks/cmake

--- requirement-checks/cmake.sh
+++ requirement-checks/cmake.sh
@@ -9,13 +9,16 @@ version="$(
 IFS="." read -r major minor _patch <<<"$version"
 
 if [[ -z $major ]]; then
-	echo "error: failed to determine cmake version"
+	echo  "error: failed to determine cmake version"
+	echo "tip: run autobuild with --install-cmake to install an isolated compatible version"
 	exit 1
 elif [[ $major -gt 3 ]]; then
-	echo 1>&2 "error: your cmake version is too new: $version; required version is cmake < 4.0.0"
-	echo 1>&2 "sadly, cmake 4 is not compatible with some projects"
+	echo  "error: your cmake version is too new: $version; required version is cmake < 4.0.0"
+	echo  "sadly, cmake 4 is not compatible with some projects"
+	echo "tip: run autobuild with --install-cmake to install an isolated compatible version"
 	exit 1
 elif [[ $major -lt 3 || ${minor:-0} -lt 22 ]]; then
 	echo "error: your cmake version is too old: $version; required cmake >= 3.22.0"
+	echo "tip: run autobuild with --install-cmake to install an isolated compatible version"
 	exit 1
 fi
